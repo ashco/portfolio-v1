9a08bd54bdca256908583a7b2da859d306877b82
ashco <ashchristie@gmail.com>
2018-01-20 14:59:25 -0800

    inital page creation. Navbar media set

---
+++ source.js
@@ -0,0 +1,12 @@
+// Initialize collapse button
+// $(".button-collapse").sideNav();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+//$('.collapsible').collapsible();
+
+$('.button-collapse').sideNav({
+	menuWidth: 60, // Default is 300
+	}
+);
+
+
+// @960px have nav go to top

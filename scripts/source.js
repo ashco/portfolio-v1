ad2e520baff87db7d28321305586c88c37d601d9
ashco <ashchristie@gmail.com>
2018-01-20 19:49:45 -0800

    styling nav bar

--- scripts/source.js
+++ scripts/source.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
+
+	//Page navigation
 	$('.main').fullpage({
 		anchors:['home', 'about', 'portfolio', 'contact']
 	});
+
+	//Nav icon change
+	$('.navbar__link').hover(() => {
+			$('.navbar__link').css('color', '#fff');
+			}, () => {
+			$('.navbar__link').css('color', 'rgb(54, 190, 145)');
+		console.log(this)
+	});
+
+	// $('section.active')
+
+
+
+
+
+
+
+
 });
+

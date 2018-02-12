7fcefd47ededd4c0524f7034897a68f42f29fbab
Ashton <ashchristie@gmail.com>
2018-02-11 18:49:21 -0800

    added hover to portolio

--- scripts/source.js
+++ scripts/source.js
@@ -7,14 +7,14 @@ $(document).ready(function() {
 
 	//Nav icon change
 	$('.navbar__link').hover(() => {
-			$('.navbar__link').css('color', 'rgb(54, 190, 145)');
-			}, () => {
-			$('.navbar__link').css('color', '#fff');
-		// console.log(this);
+		$('.navbar__link').css('color', 'rgb(54, 190, 145)');
+		}, () => {
+		$('.navbar__link').css('color', '#fff');
 	});
 
-	// $('section.active')
-
+	$('.box--solitaire').click(function(){
+		console.log('trigger')
+	});
 
 
 
